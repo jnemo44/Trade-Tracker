c4646a2fbe29a82fc5ea23d322c01abc0038cc04
jnemo44 <64214120+jnemo44@users.noreply.github.com>
2020-05-22 14:53:33 -0400

    Added baseline files and made a first cut of the required models for the db

--- app.py
+++ app.py
@@ -2,22 +2,24 @@ import os
 from flask import Flask, request, abort, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+from models import setup_db
+
+
 
 def create_app(test_config=None):
   # create and configure the app
   app = Flask(__name__)
+  setup_db(app)
   CORS(app)
 
-  return app
-
-APP = create_app()
+#APP = create_app()
 
 '''
 # Hosted deployment
 if __name__ == '__main__':
     APP.run(host='0.0.0.0', port=8080, debug=True)
 '''
-@APP.route('/')
+@app.route('/')
 def home_page():
     return jsonify ({
         'We are the champions':True
