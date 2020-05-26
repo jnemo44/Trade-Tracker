8193162002d795b5a66e575ac03a63d4df36a3a5
jnemo44 <64214120+jnemo44@users.noreply.github.com>
2020-05-26 16:15:07 -0400

    Added some functionality to the order stats endpoint and added some data to the dummy data to validate new code

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+import decimal
 from flask import Flask, request, abort, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
@@ -30,15 +31,51 @@ def open_orders():
 
 @app.route('/close-orders', methods=['GET'])
 def close_orders():
+  available_orders = Close.query.all()
+  current_trades = [orders.closing_trade() for orders in available_orders]
+
   return jsonify ({
-    'success':True
+    'success':True,
+    'close_list':current_trades
   })
 
 @app.route('/order-stats', methods=['GET'])
-@requires_auth('get:order-stats')
+#@requires_auth('get:order-stats')
 def order_stats():
+  num_opened = 0
+  totals = {}
+  total_profit = 0
+
+  # Retrieve all records (Is this necessary?)
+  open_orders = Open.query.all()
+  close_orders = Close.query.all()
+
+  while num_opened < len(open_orders):
+    close_price = db.session.query(Close.close_price, Close.number_contracts).filter(Close.open_id==open_orders[num_opened].id).all()    
+    ticker_profit = open_orders[num_opened].open_price
+    # For every open trade subtract cost of closing trade
+    for cost in close_price:
+      # The number of contracts closed serves as a multiplier
+      contracts_closed = close_price[0][1]
+      if open_orders[num_opened].buy_sell == 'sell':
+        ticker_profit -= cost[0] * contracts_closed
+      else:
+        ticker_profit += cost[0] * contracts_closed
+    #Convert decimal to a str for JSON
+    totals[open_orders[num_opened].ticker] = str(ticker_profit)
+    num_opened+=1
+
+  # Calculate total profit
+  for v in totals.values():
+    total_profit += float(v)
+
   return jsonify ({
-    'success':True
+    'success':True,
+    'open_orders':len(open_orders),
+    'close_orders':len(close_orders),
+    'ticker_profit':totals,
+    'total_profit':str(round(total_profit,2))
+
   })
 
 @app.route('/open-orders', methods=['POST'])
