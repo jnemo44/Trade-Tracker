0d65fbbbc9e3b0d3139a866dd91b5b05938d8b01
jnemo44 <64214120+jnemo44@users.noreply.github.com>
2020-06-05 21:46:04 -0500

    Include windows ps commands for enviro variables

--- setup.sh
+++ setup.sh
@@ -2,3 +2,10 @@ export AUTH0_DOMAIN="fsnd-44-2.auth0.com"
 export API_AUDIENCE="orders"
 export ALGORITHMS=["RS256"]
 export DATABASE_URL="postgres://vbkoxvjqmfxxhb:ee9c83afa1f1d72d379c56aca8ce2c3fe176c548a6fbef0b24898437fdb01fee@ec2-18-215-99-63.compute-1.amazonaws.com:5432/d4jgjjkc5k38rv"
+
+```
+Windows PS
+$env:AUTH0_DOMAIN = '"fsnd-44-2.auth0.com"'
+$env:ALGORITHMS = '["RS256"]'
+$env:API_AUDIENCE = '"orders"'
+```
